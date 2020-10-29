05a8a6588283b2d54cfe0c6291df6accad294c76
Casey Mok <casey_mok@live.com>
2020-10-29 13:35:04 -0400

    initial commit

---
+++ hello.py
@@ -0,0 +1,18 @@
+import os
+os.system('clear')
+
+print('Hello World')
+
+full_name = 'Casey Mok'
+print(full_name)
+
+names = ["John", "Bob","Mary"]
+print(names[0])
+
+fav_pizza = {
+	"John":"Pepperoni",
+	"Bob":"Mushroom",
+	"Mary":"Cheese"
+}
+
+print(fav_pizza["John"])
